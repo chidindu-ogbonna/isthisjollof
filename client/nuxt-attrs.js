609acb7068481e528cd1c99c9a93610e867911ac
Chidindu Promise Ogbonna <promise.bones@gmail.com>
2020-11-01 22:24:09 +0100

    update(client)

--- client/nuxt-attrs.js
+++ client/nuxt-attrs.js
@@ -43,13 +43,13 @@ export const meta = [
     hid: 'twitter:image',
     name: 'twitter:image',
     content:
-      'https://res.cloudinary.com/cheapflix/image/upload/v1604255636/isthisjollof-card.png',
+      'https://res.cloudinary.com/cheapflix/image/upload/v1604265689/isthisjollof-card.png',
   },
   {
     hid: 'og:image',
     property: 'og:image',
     content:
-      'https://res.cloudinary.com/cheapflix/image/upload/v1604255636/isthisjollof-card.png',
+      'https://res.cloudinary.com/cheapflix/image/upload/v1604265689/isthisjollof-card.png',
   },
   {
     hid: 'twitter:image:alt',
